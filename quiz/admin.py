fd387c8219ebf29382a98e3b78c3e5f56ff1f9db
Jasmine Massey <jazminemazzey@gmail.com>
2022-07-11 18:32:46 +0530

    registering model

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Question, Quizzes, Answer, Category
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name',] 
+
+@admin.register(Quizzes)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['id','title',] 
+
+class AnswerInlineModel(admin.TabularInline):
+    model = Answer
+    fields = ['answer_text', 'is_right']
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['title', 'quiz']
+    list_display = ['title', 'quiz', 'date_updated']
+
+    inlines = [AnswerInlineModel, ]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['answer_text', 'is_right', 'question']
+
+
